70e2d5109104703df2c2fa4b749be371d641e846
Kiage24 <kiagebrenda@gmail.com>
2022-01-18 17:50:28 +0300

    Updated

--- scripts/rnaseq.sh
+++ scripts/rnaseq.sh
@@ -13,5 +13,20 @@ for sample in $(ls *fastq | cut -d _ -f 1):
 do
  R1= ${sample}_1.fastq
  R2=${sample}_2.fastq
+ fastqc ${R1} ${R2}
  hisat2 --max-introlen 10000 -x ${ref}.idx -1 ${R1} -2 ${R2} -S ./results/${sample}.sam
-done ; 
+ 
+
+ #Convert the file to bam
+ #echo 'Converting sam to bam and generating indexed files'
+ 
+ samtools view -O bam ${sample}.sam -o ${sample}.bam
+ samtools sort -O BAM ${sample}.bam -o ${sample}_sorted
+ samtools index ${sample}_sorted
+ 
+ #Quantification
+ #echo 'Indexing and quantification'
+ ref = transcript.fa
+ kallisto index ${ref} -i ${ref}.idx
+ kallisto quant -i ${ref}.idx -o ${sample} -b 100 ${R1} ${R2} 
+ 
