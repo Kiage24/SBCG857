65df3403f5cf9de21f021cbe79909e5e3d54ae7a
Kiage24 <kiagebrenda@gmail.com>
2022-01-25 08:25:58 +0300

    updated

--- scripts/alignment.sh
+++ scripts/alignment.sh
@@ -33,6 +33,9 @@ samtools sort -T temp -O bam -o md5638.sorted.bam md5638.bam
 
 samtools index md5638_sorted.bam
 
+#echo'Merging PCR duplicates'
+picard-tools MergeSamFiles
+ 
 #echo 'Marking PCR duplicates'
 
 picard MarkDuplicates -I hilda_sorted.bam -M hilda_markdup_metrix.txt -O hilda_markdup.bam
@@ -51,4 +54,53 @@ plot-bamstats -p md5639_plot md5638.markdup.stats
 
 firefox *.html
 
-#echo 'Opening 
+#echo 'Opening  igv'
+
+igv
+#echo 'generate plots'
+
+samtools stats -r ref bam>stats
+plot bamstats -r ref  -p graphs/index.html
+firefox index.html
+
+#echo 'read each bases aligned to reference'
+
+samtools mpileup -f GRCm38_68.19.fa A_J.bam | less -S
+bcftools mpileup -f GRCm38_68.19.fa A_J.bam | less -S
+ #chr,pos,refbase,readdepth,read bases(./,match)(acgtn/mismatch),base quals(^,start,$end,* end).
+
+#echo 'generating genotype likelihoods and calling variants'
+
+bcftools mpileup -f GRCm38_68.19.fa A_J.bam | bcftools call -m | less -S
+
+#echo 'addition of tags'
+
+bcftools mpileup -a?
+
+
+#echo'variant filtering'
+
+bcftools query -f'POS = %POS\n' out.vcf | head
+
+#echo'obtaining ts/tv ratio'
+
+bcftools stats out.vcf | grep TSTV | cut -f5
+
+#echo 'filter'
+
+bcftools filter -sLowQual -m+ -i'QUAL>=30 && AD[1]>=25' -g8 -G10 out.vcf -o out.flt.vcf
+
+#echo'normalization'
+
+bcftools norm
+
+#echo 'multisample variant calling'
+
+
+#echo'annotate'
+
+bcftools csq -i 'FILTER="PASS"' -p m -f GRCm38_68.19.fa -g Mus_musculus.part.gff3.gz multi.filt.bcf -o multi.
+
+#echo 'visualise'
+ 
+igv
